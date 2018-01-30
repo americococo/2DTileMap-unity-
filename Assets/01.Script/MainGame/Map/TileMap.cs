bca4b818f6f4d96b6141a89cf2b01b0aa6d5d6a2
americococo <tmdrb515060@gamail.com>
2018-01-30 23:14:15 +0900

    맵 랜덤 생성 및 길찾기 오류수정

--- Assets/01.Script/MainGame/Map/TileMap.cs
+++ Assets/01.Script/MainGame/Map/TileMap.cs
@@ -15,7 +15,8 @@ public class TileMap : MonoBehaviour
     public void Init()
     {
         _sprityArray = Resources.LoadAll<Sprite>("Sprites/MapSprite");
-        CreateTiles();
+        //CreateTiles();
+        CreateRandomMaze();
     }
 
     void Update()
@@ -31,6 +32,168 @@ public class TileMap : MonoBehaviour
 
     int _width;
     int _height;
+    void CreateRandomMaze()
+    {
+        float tileSize = 32.0f;
+
+
+        //1층
+        TextAsset scriptAsset = Resources.Load<TextAsset>("Data/Map1MapData_layer1");
+        string[] records = scriptAsset.text.Split('\n');    //레코드 받아옴
+
+        {
+            string[] Token = records[0].Split(','); //레코드 토큰으로 쪼갬
+            _width = int.Parse(Token[1]);
+            _height = int.Parse(Token[2]);
+
+        }
+        _tileCellList = new TileCell[_height, _width];
+
+        for (int y = 0; y < _height; y++)
+        {
+            int line = y + 2;
+            string[] Token = records[line].Split(',');
+            for (int x = 0; x < _width; x++)
+            {
+                int spriteIndex = int.Parse(Token[x]);
+
+                GameObject tileGameObject = GameObject.Instantiate(TileObjectPrefabs);
+                tileGameObject.transform.SetParent(transform);
+                tileGameObject.transform.localScale = Vector3.one;
+                tileGameObject.transform.localPosition = Vector3.zero;
+
+                TileObject tileObject = tileGameObject.GetComponent<TileObject>();
+                tileObject.Init(_sprityArray[spriteIndex]);
+                tileObject.setTilePostion(x, y);
+
+                _tileCellList[y, x] = new TileCell();
+                GetTileCell(x, y).Init();
+                GetTileCell(x, y).SetPosition(x * tileSize / 100.0f, y * tileSize / 100.0f);
+                GetTileCell(x, y).SetTilePosition(new Vector2(x, y));
+                GetTileCell(x, y).AddObject(eTileLayer.GROUND, tileObject);
+            }
+        }
+
+
+        for (int y = 0; y < _height; y++)
+        {
+            if (0 == (y % 2))
+            {
+                for (int x = 0; x < _width; x++)
+                {
+                    if (0 == (x % 2))
+                    {
+                        int spriteIndex = 63;
+
+                        GameObject tileGameObject = GameObject.Instantiate(TileObjectPrefabs);
+                        tileGameObject.transform.SetParent(transform);
+                        tileGameObject.transform.localScale = Vector3.one;
+                        tileGameObject.transform.localPosition = Vector3.zero;
+
+                        TileObject tileObject = tileGameObject.GetComponent<TileObject>();
+                        tileObject.Init(_sprityArray[spriteIndex]);
+                        tileObject.SetCanMove(false);
+                        tileObject.setTilePostion(x, y);
+                        GetTileCell(x, y).AddObject(eTileLayer.GROUND, tileObject);
+                    }
+                }
+            }
+
+        }
+        //기둥 세운후 가지치기
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                if (false == GetTileCell(x, y).CanMove())
+                {
+                    //연결돼지 않은 블록인 경우
+                    if (false == IsConnectedCell(x, y))
+                    {
+                        //램덤한 방향으로 블럭이 연결됄때까지 이어준다
+                        eMoveDirection direction = (eMoveDirection)Random.Range(0, (int)eMoveDirection.NONE - 1);
+                        int serchTileX = x;
+                        int serchTileY = y;
+                        while (false == IsConnectedCell(serchTileX, serchTileY))
+                        {
+                            switch (direction)
+                            {
+                                case eMoveDirection.LEFT:
+                                    serchTileX--;
+                                    break;
+                                case eMoveDirection.RIGHT:
+                                    serchTileX++;
+                                    break;
+                                case eMoveDirection.UP:
+                                    serchTileY++;
+                                    break;
+                                case eMoveDirection.DOWN:
+                                    serchTileY--;
+                                    break;
+                            }
+                            if (0 <= serchTileX && serchTileX < _width && 0 <= serchTileY && serchTileY < _height)
+                            {
+                                //새로운 블록을 심어줌
+                                int spriteIndex = 63;
+
+                                GameObject tileGameObject = GameObject.Instantiate(TileObjectPrefabs);
+                                tileGameObject.transform.SetParent(transform);
+                                tileGameObject.transform.localScale = Vector3.one;
+                                tileGameObject.transform.localPosition = Vector3.zero;
+
+                                TileObject tileObject = tileGameObject.GetComponent<TileObject>();
+                                tileObject.Init(_sprityArray[spriteIndex]);
+                                tileObject.SetCanMove(false);
+                                tileObject.setTilePostion(serchTileX, serchTileY);
+                                GetTileCell(serchTileX, serchTileY).AddObject(eTileLayer.GROUND, tileObject);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+    }
+
+    bool IsConnectedCell(int tileX, int tileY)
+    {
+        //주변에 하나라도 붙은 블럭이 있으면 연굘됀 블럭
+        for (int direction = (int)eMoveDirection.LEFT; direction < (int)eMoveDirection.NONE; direction++)
+        {
+            int serchTileX = tileX;
+            int serchTileY = tileY;
+
+            switch ((eMoveDirection)direction)
+            {
+                case eMoveDirection.LEFT:
+                    serchTileX--;
+                    break;
+                case eMoveDirection.RIGHT:
+                    serchTileX++;
+                    break;
+                case eMoveDirection.UP:
+                    serchTileY++;
+                    break;
+                case eMoveDirection.DOWN:
+                    serchTileY--;
+                    break;
+            }
+            if (0 <= serchTileX && serchTileX < _width && 0 <= serchTileY && serchTileY < _height)
+            {
+                if (false == GetTileCell(serchTileX, serchTileY).CanMove())
+                    return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+        return false;
+    }
 
     void CreateTiles()
     {
